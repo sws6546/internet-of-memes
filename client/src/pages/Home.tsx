7c07c28de6207bf1cf7fdae09ca79d548e9fca08
Your Name <you@example.com>
2025-04-04 08:40:18 +0200

    Automatic redirect from home page into main page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router"
+import { Category } from "../types";
+
 export default function Home() {
+  const navigate = useNavigate();
+  const { isError, isPending, data, error } = useQuery({
+    queryKey: ['listOfCategories'], queryFn: async () => {
+      const url = `${import.meta.env.VITE_BACKEND_MAINURL}/category/getAll`
+      const categories: Category[] = await (await axios.get(url)).data
+      return categories
+    }
+  })
+
+  useEffect(() => {
+    if (!isPending && !isError) {
+      navigate(`category/${data[0].pathName}/${data[0].id}`)
+    }
+  }, [data])
   return (
-    <p></p>
+    <p className="text-red-500">{error?.message}</p>
   )
 }
